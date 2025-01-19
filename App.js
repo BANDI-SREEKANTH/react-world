719112405821b00d569e3fe20987b2fcbd5a83ee
Sreekanth Bandi <sreekanthbandi2468@gmail.com>
2025-01-19 19:01:59 +0530

    Imported react and react-dom modules instead of React CDN links

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 const heading = React.createElement("div",{id:"parent"},
     [React.createElement("div",{id:"child1"},[
         React.createElement("h1",{id:"h1"},"h1 from child1"),
@@ -8,5 +10,6 @@ const heading = React.createElement("div",{id:"parent"},
         React.createElement("h2",{id:"h2"},"h2 from child2"),
     ]),
 ]);
+console.log(heading);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
